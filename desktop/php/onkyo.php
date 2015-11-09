221deed92a75e84b7cf97d2c06166ecdb7914049
Damien alias Dough <dough29@gmail.com>
2015-11-09 09:52:41 +0100

    Améliorations cosmétiques

--- desktop/php/onkyo.php
+++ desktop/php/onkyo.php
@@ -3,19 +3,41 @@ if (!isConnect('admin')) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
 sendVarToJS('eqType', 'onkyo');
+$eqLogics = eqLogic::byType('onkyo');
 ?>
 <div class="row row-overflow">
-	<div class="col-md-2">
+	<div class="col-lg-2 col-md-3 col-sm-4">
 		<div class="bs-sidebar">
 			<ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
 				<a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un ampli}}</a>
 				<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
 				<?php
-				foreach (eqLogic::byType('onkyo') as $eqLogic) {
-					echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName() . '</a></li>';
+				foreach ($eqLogics as $eqLogic) {
+					$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+					echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"  style="' . $opacity . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
 				}
 				?>
 			</ul>
+		</div>
+	</div>
+	<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+		<legend>{{Mes amplis}}</legend>
+		<div class="eqLogicThumbnailContainer">
+			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+				<center><i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i></center>
+				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+			</div>
+			<?php
+			foreach ($eqLogics as $eqLogic) {
+				$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+				echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
+				echo "<center>";
+				echo '<img src="plugins/onkyo/doc/images/onkyo_icon.png" height="105" width="95" />';
+				echo "</center>";
+				echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+				echo '</div>';
+			}
+			?>
 		</div>
 	</div>
 	<div class="col-md-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
@@ -42,16 +64,13 @@ sendVarToJS('eqType', 'onkyo');
 						</select>
 					</div>
 				</div>
-				<div class="form-group">
-					<label class="col-md-2 control-label" >{{Activer}}</label>
-					<div class="col-md-1">
-						<input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" size="16" checked/>
+                <div class="form-group">
+					<label class="col-sm-2 control-label" ></label>
+					<div class="col-sm-9">
+						<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
+						<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
 					</div>
-					<label class="col-md-2 control-label" >{{Visible}}</label>
-					<div class="col-md-1">
-						<input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>
-					</div>
-				</div>
+                </div>
 				<div class="form-group">
 					<label class="col-md-2 control-label">{{Adresse IP}}</label>
 					<div class="col-md-3">
@@ -61,6 +80,22 @@ sendVarToJS('eqType', 'onkyo');
 			
 			</fieldset> 
 		</form>
+		<legend>Commandes</legend>
+		<a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
+		<table id="table_cmd" class="table table-bordered table-condensed">
+			<thead>
+				<tr>
+					<th style="width: 50px;">{{ID}}</th>
+					<th style="width: 200px;">{{Nom}}</th>
+					<th style="width: 200px;">{{Type}}</th>
+					<th>{{Options}}</th>
+					<th style="width: 100px;">{{Paramètres}}</th>
+					<th style="width: 150px;"></th>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>
+		</table>
 		<form class="form-horizontal">
 			<fieldset>
 				<div class="form-actions">
