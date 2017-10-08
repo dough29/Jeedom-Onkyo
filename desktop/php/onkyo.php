456735f6334d5f76211711c372ddbf0bb1045fe3
Dough29 <dough29@gmail.com>
2017-10-08 11:50:39 +0200

    Amélioration de l'écran de gestion du plugin avec accès direct à l'écran "Configuration"

--- desktop/php/onkyo.php
+++ desktop/php/onkyo.php
@@ -23,14 +23,24 @@ $eqLogics = eqLogic::byType($plugin->getId());
 	</div>
 	
 	<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-		<legend><i class="icon nature-planet5"></i> {{Mes amplificateurs Onkyo}}</legend>
+		<legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
 		<div class="eqLogicThumbnailContainer">
 			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
 				<center>
-					<i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+					<i class="fa fa-plus-circle" style="font-size : 7em;color:#3F23A4;"></i>
 				</center>
-				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+				<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>Ajouter</center></span>
 			</div>
+			<div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+				<center>
+					<i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+				</center>
+				<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
+			</div>
+		</div>
+
+		<legend><i class="icon nature-planet5"></i> {{Mes amplificateurs Onkyo}}</legend>
+		<div class="eqLogicThumbnailContainer">
 			<?php
 			foreach ($eqLogics as $eqLogic) {
 				$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
